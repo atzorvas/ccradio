ad477a4a901844c33a704b5222ac918129f690e4
atzorvas <antonis@tzorvas.com>
2015-08-08 22:49:09 +0300

    add seeds for streams

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed Streams
+streams = {
+  0 => "Everything",
+  1 => "Alternative",
+  2 => "Electronic",
+  3 => "Jazz",
+  4 => "Pop",
+  5 => "Rock",
+  6 => "Jazz + Electronic",
+  7 => "Jazz + Rock",
+  8 => "Electronic + Pop",
+  9 => "Rock + Alternative"
+}
+
+streams.each do |mountId, title|
+  Stream.create!(title: title,
+                 mount: "/live#{mountId}",
+                 server: "http://stream.creativecommons.gr:8000/")
+end
