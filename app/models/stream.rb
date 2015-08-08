0ea0c1de8a56a98c3d5ce77ae50e54e4080bbb33
atzorvas <antonis@tzorvas.com>
2015-08-08 23:10:34 +0300

    add stream fixtures, fix mount validation, add unit-tests for validations

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,6 @@
 class Stream < ActiveRecord::Base
   validates :title, :server, :mount, presence: true
   validates :title, uniqueness: true
-  validates :mount, uniqueness: true
+  validates :mount, uniqueness: { scope: :server,
+                                  message: "should exist once per server" }
 end
--- test/models/stream_test.rb
+++ test/models/stream_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class StreamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :streams
+
+  test "stream attributes must not be empty" do
+    stream = Stream.new
+    assert stream.invalid?
+    assert stream.errors[:title].any?
+    assert stream.errors[:server].any?
+    assert stream.errors[:mount].any?
+  end
+
+  test "stream title uniqueness" do
+    rock = streams(:rock)
+    stream = Stream.new(title: rock.title,
+                        server: "http://newserver.com/",
+                        mount: "/newmount0"
+                        )
+    assert stream.invalid?
+    assert_equal stream.errors.count, 1
+    assert stream.errors[:title]
+  end
+
+  test "stream (server, mount) uniqueness" do
+    rock = streams(:rock)
+    stream = Stream.new(title: "New Genre",
+                        server: rock.server,
+                        mount: rock.mount
+                        )
+    assert stream.invalid?
+    assert stream.errors[:mount].any?
+  end
 end
