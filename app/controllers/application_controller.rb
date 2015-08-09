6ff5be7f5ae0612af31a8b4eab1eb5d16ffdbf18
atzorvas <antonis@tzorvas.com>
2015-08-09 18:12:43 +0300

    fix auth req

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def authenticate_admin
-    redirect_to root_path, alert: 'Access Denied' unless current_user.is_admin?
+    redirect_to root_path, alert: 'Access Denied' unless current_user && current_user.is_admin?
   end
 end
