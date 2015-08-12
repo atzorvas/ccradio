35b663e86b377026af325c857a6caa4b7d2b0b99
atzorvas <antonis@tzorvas.com>
2015-08-12 16:25:02 +0300

    add devise-async

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_create :set_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :async, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
   def is_admin?
