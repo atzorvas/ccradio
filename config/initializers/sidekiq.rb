a69e066d43fb6a4ec6d57e1dce2bdff467789dc3
atzorvas <antonis@tzorvas.com>
2015-08-12 21:17:15 +0300

    heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,5 @@
 require 'sidekiq'
 
-ENV["REDISTOGO_URL"] ||= "http://localhost:6379"
-
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
-end
-
-Sidekiq.configure_server do |config|
-  # The config.redis is calculated by the
-  # concurrency value so you do not need to
-  # specify this. For this demo I do
-  # show it to understand the numbers
-  config.redis = { :size => 9 }
+  config.redis = { size: 3, url: ENV["REDIS_URL"], namespace: "ccradio" }
 end
