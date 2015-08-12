3750e99847bedf2bc1437f430bffd2e62fbfab7c
atzorvas <antonis@tzorvas.com>
2015-08-12 23:08:49 +0300

    fix

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -79,7 +79,7 @@ class StreamsController < ApplicationController
     puts "called"
     item = @stream.playlist_items.last
     respond_to do |format|
-      format.json { render :json => { song: item.song, created_at: item.created_at } if song }
+      format.json { render :json => { song: item.song, created_at: item.created_at } }
       format.html { redirect_to @stream }
     end
   end
